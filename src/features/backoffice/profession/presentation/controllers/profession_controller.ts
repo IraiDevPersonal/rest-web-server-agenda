47c98bc073760f27833bd2eab01d40424429e255
sebastian Acuna <pinilla.sebastianm@gmail.com>
2025-02-24 23:40:53 -0300

    update: se corrige controlador

--- src/features/backoffice/profession/presentation/controllers/profession_controller.ts
+++ src/features/backoffice/profession/presentation/controllers/profession_controller.ts
@@ -1,19 +1,16 @@
+import { Controllers } from "@core/domain/controllers";
 import { CustomError } from "@core/domain/custom.error";
 import { ProfessionEntity } from "@professions/domain/entities/profession_entity";
 import { ProfessionService } from "@professions/presentation/services/profession_service";
 import { Request, Response } from "express";
 
-export class ProfessionController {
+export class ProfessionController implements Controllers {
   public constructor(private readonly professionService: ProfessionService) {}
 
   public getProfessions = async (req: Request, res: Response) => {
     try {
-      const { id, name } = req.query;
-
-      const professions = await this.professionService.getMany({
-        id: id ? Number(id) : undefined,
-        name: name as string,
-      });
+      const filters = this.getFilters(req);
+      const professions = await this.professionService.getMany(filters);
 
       return res.json(professions.map(ProfessionEntity.adapter));
     } catch (error) {
@@ -22,4 +19,13 @@ export class ProfessionController {
       return CustomError.handleError(e, res);
     }
   };
+
+  getFilters(request: Request): Record<string, any> {
+    const { id, name } = request.query;
+
+    return {
+      id: id ? Number(id) : undefined,
+      name: name as string,
+    };
+  }
 }
