2ebb47666a49a426e510c84f1262fbc78c8a9acb
IraiDevPersonal <ignacio.arr01@gmail.com>
2025-08-04 19:24:24 -0400

    feat: quitando clg

--- src/app/professional/controller.ts
+++ src/app/professional/controller.ts
@@ -9,7 +9,6 @@ export class ProfessionalController {
   getProfessionals = async (req: Request, res: Response) => {
     try {
       const data = await this.useCases.getProfessionals(req.query);
-      console.log({ data });
       return res.status(200).json(data);
     } catch (error) {
       return CustomError.handleError(error, res);
