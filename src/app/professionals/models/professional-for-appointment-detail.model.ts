77f916e35d808232c16c1ee8a869ec8711c66094
IraiDevPersonal <ignacio.arr01@gmail.com>
2025-09-07 09:44:13 -0300

    upate: datos granulares para paciente y prefesionales en las citas

--- src/app/professionals/models/professional-for-appointment-detail.model.ts
+++ src/app/professionals/models/professional-for-appointment-detail.model.ts
@@ -1,5 +1,6 @@
 export type ProfessionalForAppointmentDetailModel = {
-  full_name: string;
+  names: string;
+  last_names: string;
   professions: string[];
   pay_methods: string[];
   confirm_methods: string[];
